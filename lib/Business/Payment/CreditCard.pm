c2327c777347ee24fea6497df08b988eeee7c239
Cory Watson <gphat@Crankwizzah.local>
2009-07-23 08:35:39 -0500

    Add cvn and POD to Credit Card

--- lib/Business/Payment/CreditCard.pm
+++ lib/Business/Payment/CreditCard.pm
@@ -2,15 +2,20 @@ package Business::Payment::CreditCard;
 
 use Moose;
 
-has 'number' => (
-    is       => 'rw',
-    isa      => 'Str',
-    required => 1
+has 'cvn' => (
+    is => 'rw',
+    isa => 'Num'
 );
 
 has 'expiration' => (
     is       => 'rw',
     isa      => 'DateTime',
+    required => 1
+);
+
+has 'number' => (
+    is       => 'rw',
+    isa      => 'Str',
     required => 1
 );
 
@@ -20,3 +25,52 @@ sub expdate {
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
+
+=head1 NAME
+
+Business::Payment::CreditCard - Credit Card object
+
+=head1 SYNOPSIS
+
+    use Business::Payment::CreditCard;
+
+    my $bp = Business::Payment::CreditCard->new(
+        number => '4111111111111111'
+    );
+
+=head1 DESCRIPTION
+
+Business::Payment::CreditCard provides a model for passing Credit Card
+information to a Charge.
+
+=head1 ATTRIBUTES
+
+=head2 cvn
+
+Set/Get the CVN number for this Credit Card
+
+=head2 expiration
+
+Set/Get the expiration for this Credit Card.  Expects a DateTime object,
+but can coerce a String.
+
+=head2 number
+
+Set/Get the credit card number.
+
+=head1 METHODS
+
+=head2 expdate
+
+Returns a stringified expiration date in the form of %m%y.
+
+=head1 AUTHOR
+
+Jay Shirley, C<< <jshirley@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Cold Hard Code, LLC, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
